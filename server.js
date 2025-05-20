ad28a57dc8fc3b002ce0688e40da48c6dc3066fb
Vincent Liu <42564573-krypticcs@users.noreply.replit.com>
2025-05-20 15:23:50 +0000

    Restored to 'f3c1bd403a2f7df22080b2e60411edf538ac4bfc'
    Replit-Restored-To: f3c1bd403a2f7df22080b2e60411edf538ac4bfc

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.static(__dirname));
-
 app.get('/', (req, res) => {
   res.render('index.ejs');
 })
 
-app.listen(3000);
+app.listen(3000)
