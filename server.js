d8a884e3975dffb816c7a365764f8a45b4959d9e
Vincent <88751934+kryptic-val@users.noreply.github.com>
2025-05-22 04:05:49 +0000

    create repo

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt')
+
+const users = []
+
+app.use(express.urlencoded({extended: false}))
+
+app.post("/register", async(req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.registerpassword, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.registerusername,
+      email: req.body.registeremail,
+      password: hashedPassword,
+    })
+    console.log(users);
+    res.redirect('/')
+  } catch (e) {
+    console.log(e);
+    res.redirect('/')
+  }
+})
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
 })
 
+console.log(users);
 app.listen(3000)
