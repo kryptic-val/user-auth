d6c12dfe7c3cf20114f7a3322ed5a5d07cd22fb0
Vincent <88751934+kryptic-val@users.noreply.github.com>
2025-06-12 08:23:02 +0000

    user auth works, played with mongodb + yarn, add skulpt libraries

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+//const mongoose = require('mongoose')
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt')
@@ -9,6 +10,17 @@ const passport = require('passport')
 const initializePassport = require("./passport-config")
 const flash = require('express-flash')
 const session = require('express-session')
+const methodOverride = require('method-override')
+
+/*mongoose
+.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("DB Connected");
+})
+.catch((err) => console.log(err));*/
 
 initializePassport(
   passport,
@@ -27,18 +39,19 @@ app.use(session({
 }))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(methodOverride('_method'))
 
-app.post("/login", passport.authenticate('local', {
+app.post("/login", checkNotAuthenticated, passport.authenticate('local', {
   successRedirect: '/games',
   failureRedirect: '/',
   failureFlash: true
 }))
 
-app.post("/register", async(req, res) => {
+app.post("/register", checkNotAuthenticated, async(req, res) => {
   try {
     const existingUser = users.find(user => user.name === req.body.registerusername)
     if (existingUser) {
-      req.flash('error', 'Username is already taken')
+      req.flash('error', 'Username taken')
       return res.redirect('/')
     }
     const hashedPassword = await bcrypt.hash(req.body.registerpassword, 10)
@@ -56,13 +69,34 @@ app.post("/register", async(req, res) => {
   }
 })
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', { messages: req.flash() });
+app.get('/', checkNotAuthenticated, (req, res) => {
+  res.render('index.ejs', {messages: req.flash()});
 })
 
-app.get('/games', (req, res) => {
-  res.render('games.ejs');
+app.get('/games', checkAuthenticated, (req, res) => {
+  res.render('games.ejs', {name: req.user.name});
 })
+
+app.delete("/logout", (req, res) => {
+    req.logout(req.user, err => {
+        if (err) return next(err)
+        res.redirect("/")
+    })
+})
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/games')
+  }
+  next()
+}
 
 console.log(users);
 app.listen(3000)
